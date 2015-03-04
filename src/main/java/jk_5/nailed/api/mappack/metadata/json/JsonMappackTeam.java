88dfb1b5183b0cb0c7481f5f47db85107ccca05c
jk-5 <jeffreykog@me.com>
2015-03-04 21:43:04 +0100

    Implemented FriendlyFire API

--- src/main/java/jk_5/nailed/api/mappack/metadata/json/JsonMappackTeam.java
+++ src/main/java/jk_5/nailed/api/mappack/metadata/json/JsonMappackTeam.java
@@ -1,10 +1,10 @@
 package jk_5.nailed.api.mappack.metadata.json;
 
 import com.google.gson.JsonObject;
-
 import jk_5.nailed.api.chat.ChatColor;
 import jk_5.nailed.api.mappack.MappackConfigurationException;
 import jk_5.nailed.api.mappack.metadata.MappackTeam;
+import jk_5.nailed.api.scoreboard.Visibility;
 
 /**
  * No description given
@@ -16,6 +16,10 @@ public class JsonMappackTeam implements MappackTeam {
     private final String id;
     private final String name;
     private final ChatColor color;
+    private final boolean friendlyFire;
+    private final boolean friendlyInvisiblesInvisible;
+    private final Visibility nameTagVisibility;
+    private final Visibility deathMessageVisibility;
 
     public JsonMappackTeam(JsonObject json) throws MappackConfigurationException {
         if(!json.has("id")) throw new MappackConfigurationException("Invalid team. Team doesn't have an id");
@@ -25,6 +29,10 @@ public class JsonMappackTeam implements MappackTeam {
         id = json.get("id").getAsString();
         name = json.get("name").getAsString();
         color = ChatColor.getByName(json.get("color").getAsString());
+        friendlyFire = !json.has("friendlyFire") || json.get("friendlyFire").getAsBoolean();
+        friendlyInvisiblesInvisible = !json.has("friendlyInvisiblesInvisible") || json.get("friendlyInvisiblesInvisible").getAsBoolean();
+        nameTagVisibility = json.has("nameTagsVisible") ? Visibility.getByName(json.get("nameTagsVisible").getAsString()) : Visibility.ALWAYS;
+        deathMessageVisibility = json.has("deathMessagesVisible") ? Visibility.getByName(json.get("deathMessagesVisible").getAsString()) : Visibility.ALWAYS;
 
         if(color == null) throw new MappackConfigurationException("Invalid team color '" + json.get("color").getAsString() + "'");
     }
@@ -42,5 +50,25 @@ public class JsonMappackTeam implements MappackTeam {
     @Override
     public ChatColor color() {
         return color;
+    }
+
+    @Override
+    public boolean isFriendlyFire() {
+        return friendlyFire;
+    }
+
+    @Override
+    public boolean areFriendlyInvisiblesInvisible() {
+        return friendlyInvisiblesInvisible;
+    }
+
+    @Override
+    public Visibility getNameTagVisibility() {
+        return nameTagVisibility;
+    }
+
+    @Override
+    public Visibility getDeathMessageVisibility() {
+        return deathMessageVisibility;
     }
 }
